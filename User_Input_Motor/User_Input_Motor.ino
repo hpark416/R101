2a8b01939b9a965fa248cdb6faec0750a12ea1a9
NatashaSetiadi <natset456@gmail.com>
2022-11-11 20:06:39 -0500

    added LED code

--- User_Input_Motor/User_Input_Motor.ino
+++ User_Input_Motor/User_Input_Motor.ino
@@ -1,6 +1,10 @@
 //Code written by Juan F Martinez
 //Shared to and Modified for Medical Robotics Club at GT
 //Editor: Hyungjun Park{RICHY} and Natasha Setiadi on 10/09/2022
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
 #define PWM1 2
 #define PWM2 3
@@ -8,11 +12,23 @@
 #define PHASE1 52
 #define PHASE2 50
 #define PHASE3 48
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN     9
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT  8
+
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+
 char userInput;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pixels.begin();
 }
 
 void loop() {
@@ -61,5 +77,10 @@ if(userInput == 'x'){
 analogWrite(PWM3, 255);
 digitalWrite(PHASE3, LOW);
 }
+for(int i=0;i<LED_COUNT;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,100,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
 }
 
